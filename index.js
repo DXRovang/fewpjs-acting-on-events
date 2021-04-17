96df645945240902cc50ed1cd48e04be33e3f830
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-17 12:09:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function moveDodgerLeft(){
+  let dodger = document.getElementById("dodger"); 
+  dodger.style.left = "170px";
+}
+
+function moveDodgerRight(){
+  let dodger = document.getElementById("dodger"); 
+  dodger.style.left = "190px";
+}
